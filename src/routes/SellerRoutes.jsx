d7046ca9338e5093702aaa591085a8c72055ff22
rejath-chandran <rajatchandran13@gmail.com>
2024-01-19 10:51:36 +0530

    latest friday

--- src/routes/SellerRoutes.jsx
+++ src/routes/SellerRoutes.jsx
@@ -5,10 +5,11 @@ import SellerProduct from "../screens/seller/SellerProduct";
 import SellerAuction from "../screens/seller/SellerAuction";
 import SellerWallet from "../screens/seller/SellerWallet";
 import SellerOrder from "../screens/seller/SellerOrder";
-
+import { Navigate } from "react-router-dom";
 export const SellerRoutes = () => {
   return (
     <Routes>
+      <Route path="*" element={<Navigate to="/seller"/>} />
       <Route path="/seller" element={<SellerLayout />}>
         <Route index element={<SellerProduct />} />
         <Route path="auction" element={<SellerAuction />} />
